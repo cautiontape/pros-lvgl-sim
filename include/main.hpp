cb29bde3709eff238274340c5e91519b87de78ce
陈昱安 <31612534@qq.com>
2020-01-25 14:09:13 +0800

    update
    Signed-off-by: 陈昱安 <31612534@qq.com>

--- include/main.hpp
+++ include/main.hpp
@@ -13,12 +13,7 @@
 #include "ncrapi/userDisplay/userDisplay.hpp"
 #include <SDL2/SDL.h>
 #include <memory>
-#if defined(_WIN32) || defined(_WIN64)
-#include <Windows.h>
-#else
-#include <pthread.h>
-#include <unistd.h>
-#endif
+
 static lv_indev_t *kb_indev;
 extern "C" {
 LV_IMG_DECLARE(mouse_cursor_icon); /*Declare the image file.*/
